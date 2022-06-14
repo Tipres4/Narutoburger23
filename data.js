fd9abd28bb8eeb8723494a417319b2b0fae498c2
Tipres4 <81196147+Tipres4@users.noreply.github.com>
2022-06-14 14:19:18 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,38 @@
+var threads = {
+  {
+    title: "Thread 1",
+    author: "Aaron",
+    date: Date.now(),
+    content: "Thread Content",
+    comments: [
+      {
+        author: "Jack",
+        date: Date.now(),
+        content: "Hey There"
+      },
+      {
+        author: "Arthur",
+        date: Date.now(),
+        content: "Hey to you too"
+      }
+    ]
+  },
+  {
+    title: "Thread 2",
+    author: "Aaron",
+    date: Date.now(),
+    content: "Thread Content",
+    comments: [
+      {
+        author: "Jack",
+        date: Date.now(),
+        content: "Hey There"
+      },
+      {
+        author: "Arthur",
+        date: Date.now(),
+        content: "Hey to you too"
+      }
+    ]
+  }
+}
